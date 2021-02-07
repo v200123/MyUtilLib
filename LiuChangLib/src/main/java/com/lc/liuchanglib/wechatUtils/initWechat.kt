20b130d15f220d3b886b80d0f1b7b4b61e3ec9fd
v200123 <295799682@qq.com>
2021-02-07 17:11:44 +0800

    更改initData的执行位置

---
+++ LiuChangLib/src/main/java/com/lc/liuchanglib/wechatUtils/initWechat.kt
@@ -0,0 +1,28 @@
+package com.lc.liuchanglib.wechatUtils
+
+import android.content.Context
+import com.tencent.mm.opensdk.openapi.IWXAPI
+import com.tencent.mm.opensdk.openapi.WXAPIFactory
+
+/**
+ *
+ * @PackAge：com.lc.liuchanglib.wechatUtils
+ * @创建人：Lc
+ * @Desc：初始化微信的注册流程
+ * @Time: 二月
+ *
+ **/
+class initWechat private constructor() {
+    companion object {
+        public var wxApi: IWXAPI? = null
+        public fun getInstance() = initWechat()
+
+        public fun initWechat(context: Context, APP_ID: String) {
+            if (wxApi == null) {
+                wxApi = WXAPIFactory.createWXAPI(context, APP_ID, true)
+            }
+            wxApi!!.registerApp(APP_ID)
+        }
+
+    }
+}
---
+++ LiuChangLib/src/main/java/com/lc/liuchanglib/wechatUtils/shareHelp.kt
@@ -0,0 +1,14 @@
+package com.lc.liuchanglib.wechatUtils
+
+/**
+ *
+ * @PackAge：com.lc.liuchanglib.wechatUtils
+ * @创建人：Lc
+ * @Desc：
+ * @Time: 二月
+ *
+ **/
+class shareHelp {
+
+
+}
