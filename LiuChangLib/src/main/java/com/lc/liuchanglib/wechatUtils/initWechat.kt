00a4f33ca8e006936a8fd10e32aea3e506a8e362
v200123 <295799682@qq.com>
2021-03-15 10:04:09 +0800

    删除自带的微信库

--- LiuChangLib/src/main/java/com/lc/liuchanglib/wechatUtils/initWechat.kt
+++ LiuChangLib/src/main/java/com/lc/liuchanglib/wechatUtils/initWechat.kt
@@ -13,18 +13,18 @@ import com.tencent.mm.opensdk.openapi.WXAPIFactory
  *
  **/
 class initWechat private constructor() {
-    companion object {
-        public var wxApi: IWXAPI? = null
-        public fun getInstance() = initWechat()
-
-        public fun initWechat(context: Context, APP_ID: String) {
-            if (wxApi == null) {
-                wxApi = WXAPIFactory.createWXAPI(context, APP_ID, true)
-            }
-            wxApi!!.registerApp(APP_ID)
-        }
-        fun checkIsInstall()=
-                initWechat.wxApi?.isWXAppInstalled ?: throw IllegalArgumentException("请在调用该函数之前，调用initWechat方法")
-    }
+//    companion object {
+//        public var wxApi: IWXAPI? = null
+//        public fun getInstance() = initWechat()
+//
+//        public fun initWechat(context: Context, APP_ID: String) {
+//            if (wxApi == null) {
+//                wxApi = WXAPIFactory.createWXAPI(context, APP_ID, true)
+//            }
+//            wxApi!!.registerApp(APP_ID)
+//        }
+//        fun checkIsInstall()=
+//                initWechat.wxApi?.isWXAppInstalled ?: throw IllegalArgumentException("请在调用该函数之前，调用initWechat方法")
+//    }
 
 }
